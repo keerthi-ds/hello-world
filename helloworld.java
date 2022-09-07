b5a9c7aaddea14bc0313704fddbbb51b734c8fea
Keerthi <keerthi.talluri100@gmail.com>
2022-09-07 15:35:44 -0400

    Revert " Changed version of HelloWorld program"
    This reverts commit e8ce7a5152dc6dba0ba01d935f3704f68869ba30.

--- helloworld.java
+++ helloworld.java
@@ -1,10 +1,6 @@
 public class helloworld
 {
 public static void main(String[] args)
-{   System.out.println("Hello World!"); 
-    String name="George";
-    System.out.println(name);
-    int age=24;
-    System.out.println(age);
-}
+{   System.out.print("Hello World!"); 
+    }
 }
